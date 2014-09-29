c77844a3da6da128ec3593d0cdefb88432e4fa90
Markus Fischer <fisch3r@gmail.com>
2014-09-29 19:56:17 +0200

    added login with google using firebase for authentication

--- ddd-app/www/js/controllers.js
+++ ddd-app/www/js/controllers.js
@@ -1,44 +1,57 @@
-angular.module('ddd.controllers', [])
+angular.module('ddd.controllers', ['ddd.services'])
 
-    .controller('AppCtrl', function ($scope, $state, OpenFB) {
+  .controller('AppCtrl', function ($scope, $state, authService, globalData) {
 
-        $scope.logout = function () {
-            OpenFB.logout();
-            $state.go('app.login');
-        };
+    $scope.logout = function () {
+      authService.logout();
+      globalData.user.sessionKey = undefined;
+      $state.go('app.login');
+    };
 
-        $scope.revokePermissions = function () {
-            OpenFB.revokePermissions().then(
-                function () {
-                    $state.go('app.login');
-                },
-                function () {
-                    alert('Revoke permissions failed');
-                });
-        };
-
-    })
-
-    .controller('LoginCtrl', function ($scope, $location, OpenFB) {
-
-        $scope.facebookLogin = function () {
-
-            OpenFB.login('email').then(
-                function () {
-                    $location.path('/app/welcome');
-                },
-                function () {
-                    alert('Facebook login failed');
-                });
-        };
-
-    })
-
-    .controller('WelcomeCtrl', function ($scope, OpenFB) {
-
-        OpenFB.get('/me').success(function (user) {
-            $scope.user = user;
-          //user.name, user.email, user.first_name
+    $scope.revokePermissions = function () {
+      // TODO: implement this!
+      /*OpenFB.revokePermissions().then(
+        function () {
+          $state.go('app.login');
+        },
+        function () {
+          alert('Revoke permissions failed');
         });
+        */
+    };
 
-    });
+  })
+
+  .controller('LoginCtrl', function ($scope, $location, authService, globalData) {
+
+    function handleLoginResult(result) {
+      if (result.err) {
+        alert('Login failed: ' + result.err);
+      } else {
+        globalData.user.id = result.user.id;
+        globalData.user.name = result.user.displayName;
+        if (result.user.email) {
+          globalData.user.email = result.user.email;
+        } else if (result.user.thirdPartyUserData.email) {
+          globalData.user.email = result.user.thirdPartyUserData.email;
+        }
+        globalData.user.sessionKey = 'key';
+        $location.path('/app/welcome');
+      }
+    }
+
+    $scope.facebookLogin = function() {
+      authService.loginFacebook(handleLoginResult);
+    };
+
+    $scope.googleLogin = function () {
+      authService.loginGoogle(handleLoginResult);
+    };
+
+  })
+
+  .controller('WelcomeCtrl', function ($scope, globalData) {
+
+    $scope.user = { name: globalData.user.name, email: globalData.user.email };
+
+  });
