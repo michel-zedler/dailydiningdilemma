2d9bfb611b5820f563e1a5d0f3d71f7420fa7f54
Nils Eckert <nils.eckert@eckert-partner.it>
2014-10-13 20:30:28 +0200

    Add support for HTTP OPTIONS to enable Browser Preflight Request

--- server-impl-jee7/src/main/java/ddd/impl/filter/SimpleCORSFilter.java
+++ server-impl-jee7/src/main/java/ddd/impl/filter/SimpleCORSFilter.java
@@ -2,6 +2,7 @@ package ddd.impl.filter;
 
 import java.io.IOException;
 
+import javax.inject.Inject;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -9,14 +10,29 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
 
 @WebFilter(urlPatterns="/api/rest/*")
 public class SimpleCORSFilter implements Filter {
 
+	@Inject
+	private Logger logger;
+	
 	public void doFilter(ServletRequest req, ServletResponse res,
 			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest servletRequest = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
+
+		if (servletRequest.getMethod().equals("OPTIONS")) {
+			logger.info("HTTP Method (OPTIONS) - Detected!");
+			// Just send a OK signal back to the browser
+			response.setStatus(HttpServletResponse.SC_OK);
+			return;
+		}
+		
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods",
 				"POST, GET, OPTIONS, DELETE");
