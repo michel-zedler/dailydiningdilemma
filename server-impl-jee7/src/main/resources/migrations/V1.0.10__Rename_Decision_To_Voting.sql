941fd94dae0a6f7732c23cede66f1d596e8702ee
Nils Eckert <nils.eckert@eckert-partner.it>
2014-10-22 13:56:53 +0200

    Fix for #19

--- server-impl-jee7/src/main/resources/migrations/V1.0.10__Rename_Decision_To_Voting.sql
+++ server-impl-jee7/src/main/resources/migrations/V1.0.10__Rename_Decision_To_Voting.sql
@@ -1,3 +1,12 @@
 RENAME TABLE decisions TO votings, decision_option_mapping TO voting_option_mapping;
+
+ALTER TABLE voting_option_mapping DROP FOREIGN KEY FK67500DC7CBADE601;
 ALTER TABLE voting_option_mapping CHANGE decision_id voting_id bigint;
+alter table voting_option_mapping add constraint FK67500DC7CBADE601 foreign key (voting_id) references votings (id);
+
+ALTER TABLE votes DROP FOREIGN KEY fk_votes_decision_option_id;
+ALTER TABLE votes DROP INDEX idx_votes_decision_option_id;
 ALTER TABLE votes CHANGE decision_option_id voting_option_id bigint;
+alter table votes 
+	add index idx_votes_voting_option_id (voting_option_id), 
+	add constraint fk_votes_voting_option_id foreign key (voting_option_id) references options (id);
